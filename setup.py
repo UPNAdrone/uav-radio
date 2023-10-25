5606c6c1eef0f2c75e0b28e75ba3af696547c85c
UPNAdrone <136073538+UPNAdrone@users.noreply.github.com>
2023-10-25 08:37:37 +0200

    Add files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uav_radio',
+    version='0.1',
+    description='Library for UAV radio path loss calculations',
+    author='UPNAdrone',
+    author_email='daniel.alaez@unavarra.es',
+    url='https://github.com/UPNAdrone/uav-radio',
+    packages=find_packages(),
+)
